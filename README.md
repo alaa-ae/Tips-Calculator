37b07979abdb03d5fd886fc50a4f04db09b75a42
Alaa A.E <80849327+alaa1945@users.noreply.github.com>
2022-08-20 19:10:10 +0300

    First

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Frontend Mentor - Todo app
+# Frontend Mentor - Tip calculator app
 
-![Design preview for the Todo app coding challenge](./design/desktop-preview.jpg)
+![Design preview for the Tip calculator app coding challenge](./design/desktop-preview.jpg)
 
 ## Welcome! 👋
 
@@ -8,11 +8,11 @@ Thanks for checking out this front-end coding challenge.
 
 [Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
 
-**To do this challenge, you need a good understanding of HTML, CSS and JavaScript.**
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
 
 ## The challenge
 
-Your challenge is to build out this todo app and get it looking as close to the design as possible.
+Your challenge is to build out this tip calculator app and get it looking as close to the design as possible.
 
 You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
 
@@ -20,13 +20,7 @@ Your users should be able to:
 
 - View the optimal layout for the app depending on their device's screen size
 - See hover states for all interactive elements on the page
-- Add new todos to the list
-- Mark todos as complete
-- Delete todos from the list
-- Filter by all/active/complete todos
-- Clear all completed todos
-- Toggle light and dark mode
-- **Bonus**: Drag and drop to reorder items on the list
+- Calculate the correct tip and total cost of the bill per person
 
 Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
 
--- app.js
+++ app.js
@@ -1,161 +1,63 @@
-// add the cross icon to the div
-let myNodelist = document.getElementsByTagName("LI");
-for (let i = 0; i < myNodelist.length; i++) {
-  let img = document.createElement("img");
-  img.src = "https://img.icons8.com/fluency-systems-regular/17/000000/x.png";
-  img.className = "close";
-  myNodelist[i].appendChild(img);
+//Getting the values of the input fields
+let bill_Value = document.getElementById("bill");
+let tip_btn = document.getElementsByClassName("tip-btn");
+let custom_tip = document.getElementById("custom");
+let numOfPeople = document.getElementById("number");
+let tip_amount = document.getElementById("tipAmount");
+let total = document.getElementById("total");
+
+let errorMsg = document.getElementById("error_msg");
+let reset_btn = document.getElementById("reset_btn");
+
+// Get the input of the selected tip button
+let tipPercent = 0;
+for (let btn of tip_btn) {
+        btn.addEventListener('click', function () {
+                tipPercent = Number.parseInt(btn.textContent);
+        });
 }
 
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
+function cal() {
 
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    let div = this.parentElement;
-    div.remove();
-  }
+        if (+numOfPeople.value === 0) {
+                errorMsg.style.visibility = 'visible';
+                numOfPeople.style.border = "1px solid red";
+
+                reset_btn.setAttribute('disabled', true);
+        } else {
+                errorMsg.style.visibility = 'hidden';
+                numOfPeople.style.border = "1px solid  hsl(172, 67%, 45%)";
+                reset_btn.setAttribute('disabled', false);
+                reset_btn.style.opacity = 1;
+
+                if (custom_tip.value !== '') {
+                        tipPercent = +custom_tip.value;
+                }
+
+                const billAmount = +bill_Value.value;
+                const people = +numOfPeople.value;
+
+                const tip = billAmount * tipPercent / 100;
+
+                const totalamount = billAmount + (tip);
+
+                tip_amount.textContent = '$' + (tip / people).toFixed(2);
+                total.textContent = '$' + (totalamount / people).toFixed(2);
+
+                reset_btn.removeAttribute('disabled');
+        }
 }
+numOfPeople.addEventListener('input', cal);
 
+//reset btn
+function reset1() {
+        bill_Value.value = tip_btn.value = numOfPeople.value = '';
+        tip_amount.textContent = total.textContent = '$0';
+        tipPercent = 0;
 
-// Create a new list item when clicking on the "Add" button
-let count = 0;
-function newElement() {
-  var li = document.createElement("li");
-  li.className = "todo_li";
-  var div = document.createElement("div");
-  div.className = "left_li";
-  var p = document.createElement("p");
-
-
-  var input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
-  input.setAttribute("id", "check");
-  input.className = "c";
-  div.appendChild(input);
-
-  var label = document.createElement("label");
-  label.setAttribute("for", "check");
-  label.className = "checkbtn";
-
-
-
-  var checkImg = document.createElement("img");
-  checkImg.src = "./images/icon-check.svg";
-  label.appendChild(checkImg);
-
-  var inputValue = document.getElementById("myInput").value;
-
-  var t = document.createTextNode(inputValue);
-  p.appendChild(t);
-
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    let first = document.getElementById("todo_ul").appendChild(li);
-    let second = first.appendChild(div);
-    second.appendChild(label);
-    second.appendChild(p);
-  }
-  document.getElementById("myInput").value = "";
-
-
-  let addedImg = document.createElement("img");
-  addedImg.src = "https://img.icons8.com/fluency-systems-regular/17/000000/x.png";
-  addedImg.className = "close";
-
-  li.appendChild(addedImg);
-
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      let div = this.parentElement;
-      div.remove();
-    }
-  }
-  count++;
+        reset_btn.setAttribute('disabled', true);
+        reset_btn.style.opacity = 0.2;
 }
-console.log(count);
-// Remove Completed
-let completed_li = document.getElementsByClassName("todo_li");
-let checked_box = document.getElementsByClassName("c");
-
-function completed() {
-  for (let i = 0; i < 100; i++) {
-    if (checked_box[i].checked) {
-      completed_li[i].remove();
-    }
-  }
-}
-document.getElementById("clearBtn").addEventListener("click", completed);
-
-// Filter
-function showCompleted() {
-  for (let i = 0; i < completed_li.length; i++) {
-    if (checked_box[i].checked) {
-      completed_li[i].style.display = "flex";
-    } else {
-      completed_li[i].style.display = "none";
-    }
-  }
-
-}
-document.getElementById("completed").addEventListener("click", showCompleted);
-
-function showActive() {
-  for (let i = 0; i < 100; i++) {
-    if (checked_box[i].checked) {
-      completed_li[i].style.display = "none";
-
-    } else {
-      completed_li[i].style.display = "flex";
-    }
-  }
-}
-document.getElementById("active").addEventListener("click", showActive);
-
-function showAll() {
-  for (let i = 0; i < 100; i++) {
-    completed_li[i].style.display = "flex";
-  }
-}
-document.getElementById("all").addEventListener("click", showAll);
+reset_btn.addEventListener('click', reset1);
 
 
-// Toggle dark mode
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark");
-}
-
-// Count items left
-
-function setItems() {
-  let activeTodo = document.querySelectorAll(".active_check");
-  let diff = completed_li.length + activeTodo.length;
-  console.log(activeTodo);
-  let items = document.getElementById("items_left");
-  let itemsTxt = document.createTextNode(diff + " items left");
-
-  items.innerText = diff + " items left";
-
-
-}
-setItems();
-
-
-
-
-// add the last div to the stats div on desktop
-function b(x) {
-  if (x.matches) {
-    let linkdiv = document.getElementById("links");
-    let statsdiv = document.getElementById("stats");
-    let btn = document.getElementById("clearBtn");
-    linkdiv.style.cssText = "background-color:transparent; width: 20%;margin-top: 10px;"
-    statsdiv.insertBefore(linkdiv, btn);
-  }
-}
-
-var x = window.matchMedia("(min-width: 700px)")
-b(x);
-x.addListener(b);
